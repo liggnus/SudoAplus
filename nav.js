91fea81811b706219cd881e0439fd69fbd6318b8
cofm98 <118582119+cofm98@users.noreply.github.com>
2023-01-14 21:57:47 +1100

    Commit for the night. Made it a lot better.

---
+++ nav.js
@@ -0,0 +1,34 @@
+nav = `<div class="navigation">
+            <div class="dropdown">
+                <a href="team-profile.html" class="main">Team Profile</a>
+                <div class="content">
+                    <a href="team-profile.html#Charlie">Charlie</a>
+                    <a href="team-profile.html#Katherine">Katherine</a>
+                    <a href="team-profile.html#Mihail">Mihail</a>
+                    <a href="team-profile.html#Panagiotis">Panagiotis</a>
+                    <a href="team-profile.html#Samith">Samith</a>
+                    <a href="team-profile.html#Sunggil">Sunggil</a>
+                </div>
+            </div>
+            <a href="ideal-job.html">Ideal Job</a>
+            <a href="data.html">Industry Data</a>
+            <a href="work.html">IT Work</a>
+            <div class="dropdown">
+                <a href="tech.html" class="main">Technologies</a>
+                <div class="content">
+                    <a href="tech/av.html">Automatic Vehicles</a>
+                    <a href="tech/robot.html">Robots</a>
+                    <a href="tech/css.html">Clouds, Servers, and Services</a>
+                    <a href="tech/crypto.html">Cryptocurrencies</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <a href="project.html" class="main">Project Idea</a>
+                <div class="content">
+                    <a href="project.html#seg1">Segment 1</a>
+                    <a href="project.html#seg2">Segment 2</a>
+                    <a href="project.html#seg3">Segment 3</a>
+                </div>
+            </div>
+        </div>`;
+document.write(nav);
