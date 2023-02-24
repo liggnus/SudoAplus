456aba430390596de089e2cea315797ae2eaefb9
cofm98 <118582119+cofm98@users.noreply.github.com>
2023-02-24 20:13:51 +1100

    asdhkasd

--- a3/nav.js
+++ a3/nav.js
@@ -1,16 +1,16 @@
 nav = `<div class="navigation">
             <a href="../index.html">Homepage</a>
-            <a href="/a3/index.html">Team Profile</a>
+            <a href="/SudoAplus/a3/index.html">Team Profile</a>
             <div class="dropdown">
-                <a href="/a3/project/index.html" class="main">Project</a>
+                <a href="/SudoAplus/a3/project/index.html" class="main">Project</a>
                 <div class="content">
-                    <a href="/a3/project/overview.html">Overview</a>
-                    <a href="/a3/project/plansprogress.html">Plans and Progress</a>
-                    <a href="/a3/project/scopelimits.html">Scope and Limits</a>
-                    <a href="/a3/project/toolstech.html">Tools and Technology</a>
-                    <a href="/a3/project/testing.html">Testing</a>
-                    <a href="/a3/project/risks.html">Risks</a>
-                    <a href="/a3/project/group.html">Group Processes and Communications</a>
+                    <a href="/SudoAplus/a3/project/overview.html">Overview</a>
+                    <a href="/SudoAplus/a3/project/plansprogress.html">Plans and Progress</a>
+                    <a href="/SudoAplus/a3/project/scopelimits.html">Scope and Limits</a>
+                    <a href="/SudoAplus/a3/project/toolstech.html">Tools and Technology</a>
+                    <a href="/SudoAplus/a3/project/testing.html">Testing</a>
+                    <a href="/SudoAplus/a3/project/risks.html">Risks</a>
+                    <a href="/SudoAplus/a3/project/group.html">Group Processes and Communications</a>
                 </div>
             </div>
         </div>`;
